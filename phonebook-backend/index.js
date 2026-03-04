a1db8758c4e56b85425f7a313257845c7ad1ce52
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-04 17:58:54 +0530

    feat: add Express server and /api/persons route (exercise 3.1)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,1 +1,38 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Mani</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Phonebook App Server running on port ${PORT}`)
+})
