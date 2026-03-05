a49a05c755a2953403627c025d502be12ed3053b
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-05 23:02:03 +0530

    feat: add endpoint to fetch single person by id (exercise 3.3)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -41,6 +41,19 @@ app.get('/info', (request, response) => {
         `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id  = request.params.id;
+    const person = persons.find(person => person.id === id)
+    
+    if(person){
+        response.json(person);
+    }else{
+        response.status(404).end();
+    }
+
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Phonebook App Server running on port ${PORT}`)
