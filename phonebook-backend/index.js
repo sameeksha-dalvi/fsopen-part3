261cc984be9520e575340b768621ca2b48753779
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-06 17:56:05 +0530

    feat: add POST /api/persons endpoint (exercise 3.5)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": "1",
@@ -42,22 +44,51 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id  = request.params.id;
+    const id = request.params.id;
     const person = persons.find(person => person.id === id);
-    
-    if(person){
+
+    if (person) {
         response.json(person);
-    }else{
+    } else {
         response.status(404).end();
     }
 
 })
 
-app.delete('/api/persons/:id', (request, response) =>{
-    const id  = request.params.id;
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
     persons = persons.filter(person => person.id !== id);
 
     response.status(204).end();
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 10000).toString()
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    //console.log("body.name ====",body.name)
+    //console.log("body.number ====",body.number)
+
+    if (!body.name) {
+        return response.status(400).json({ error: 'name missing' })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({ error: 'number missing' })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+
 })
 
 
