061f1b9588f93507392119500b932d9e82eca433
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-09 16:32:40 +0530

    update server to use process.env.PORT for deployment on Render (exercise 3.10)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -111,7 +111,8 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Phonebook App Server running on port ${PORT}`)
 })
