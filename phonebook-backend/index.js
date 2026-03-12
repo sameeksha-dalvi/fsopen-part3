e8b7057d5d43b223539c595efcec57a20e211240
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-12 17:25:12 +0530

    feat: implement delete phonebook entries from database (exercise 3.15)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -76,10 +76,15 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(person => person.id !== id);
+    // const id = request.params.id;
+    // persons = persons.filter(person => person.id !== id);
 
-    response.status(204).end();
+    // response.status(204).end();
+
+    Person.findByIdAndDelete(request.params.id)
+    .then(result =>{
+        response.status(204).end
+    })
 })
 
 const generateId = () => {
