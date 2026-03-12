4a439b5ad9e39fad3e451dc16eda7d8dbd5517d8
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-12 17:33:36 +0530

    feat: add error handling middleware and fetch person by id from database instead of memory (exercise 3.16)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 morgan.token('body', (req) => {
-  return JSON.stringify(req.body)
+    return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -63,28 +63,38 @@ app.get('/info', (request, response) => {
         `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const person = persons.find(person => person.id === id);
+app.get('/api/persons/:id', (request, response, next) => {
+    // const id = request.params.id;
+    // const person = persons.find(person => person.id === id);
 
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+    // if (person) {
+    //     response.json(person);
+    // } else {
+    //     response.status(404).end();
+    // }
 
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     // const id = request.params.id;
     // persons = persons.filter(person => person.id !== id);
 
     // response.status(204).end();
 
     Person.findByIdAndDelete(request.params.id)
-    .then(result =>{
-        response.status(204).end
-    })
+        .then(result => {
+            response.status(204).end
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -123,15 +133,28 @@ app.post('/api/persons', (request, response) => {
     // response.json(person)
 
     const person = new Person({
-        name:body.name,
-        number:body.number,
+        name: body.name,
+        number: body.number,
     })
 
-    person.save().then(savedPerson =>{
+    person.save().then(savedPerson => {
         response.json(savedPerson)
     })
 
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
