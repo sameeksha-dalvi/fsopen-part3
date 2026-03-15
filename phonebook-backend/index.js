c6fce76f2fc9112c2f6d4832a4b2c0f1a9872168
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-15 16:08:47 +0530

    feat: add name validation to Phonebook backend and show validation error message in frontend(exercise 3.19)

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -65,7 +65,7 @@ app.get('/info', (request, response, next) => {
         `)
     }).catch(error => next(error))
 
-    
+
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -106,7 +106,7 @@ const generateId = () => {
     return Math.floor(Math.random() * 10000).toString()
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
     //console.log("body.name ====",body.name)
     //console.log("body.number ====",body.number)
@@ -145,7 +145,7 @@ app.post('/api/persons', (request, response) => {
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
-    })
+    }).catch(error => next(error))
 
 })
 
@@ -172,6 +172,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
--- phonebook-backend/models/person.js
+++ phonebook-backend/models/person.js
@@ -14,7 +14,11 @@ mongoose.connect(url, { family: 4 })
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
     number: String,
 })
 
